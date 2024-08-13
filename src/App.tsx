657798775a1f0123e94ec524e463779bdf1b81f7
raJiB <pokhrelrajib016@gmail.com>
2024-08-13 11:21:56 +0545

    completed project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,87 @@
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import './index.css'
 import words from "./wordList.json"
-import HangmanDrawing from "./components/HangmanDrawing"
+import { HangmanDrawing } from "./components/HangmanDrawing"
 import HangmanWord from "./components/HangmanWord"
 import Keyboard from "./components/Keyboard"
 
 
 function App() {
 
-  const [wordToGuess, setWordToGuess] = useState(() => {
-    return words[Math.floor(Math.random() * words.length)]
-  })
+  function getWord() {
+    return words[Math.floor(Math.random() * words.length) + 1]
+  }
+
+  const [wordToGuess, setWordToGuess] = useState(getWord())
 
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
+  const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter))
+
+  const isLoser = incorrectLetters.length >= 6;
+  const isWinner = wordToGuess.split("")
+    .every(letter =>
+      wordToGuess.includes(letter));
+
+  const addGuessedLetter = useCallback(
+    (letter: string) => {
+      if (guessedLetters.includes(letter) || isWinner || isLoser) return;
+      setGuessedLetters(currentLetters => [...currentLetters, letter])
+    }, [guessedLetters, isWinner, isLoser]
+  )
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (!key.match(/^[A-Za-z]$/)) return;
+
+      e.preventDefault();
+      addGuessedLetter(key);
+    }
+
+    document.addEventListener("keypress", handler);
+
+    return ()=>{
+      document.addEventListener("keypress", handler)
+    }
+  }, [guessedLetters])
+
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (key !== "Enter") return;
+
+      e.preventDefault();
+      setGuessedLetters([])
+      setWordToGuess(getWord())
+    }
+
+    document.addEventListener("keypress", handler)
+
+    return () => {
+      document.addEventListener("keypress", handler)
+    }
+  }, [])
+
+
+
+
+
   return (
-    <div className="max-w-md flex flex-col gap-5 m-auto justify-center items-center">
-      <div className="text-center text-3xl">Lose Win</div>
-      <HangmanDrawing />
-      <HangmanWord />
-      <Keyboard />
+    <div className="max-w-[800px] flex flex-col gap-[2rem] m-auto justify-center items-center">
+      <div className="text-center text-3xl">
+        {isWinner && "You won -- Refresh to start a new Game"}
+        {isLoser && "Nice Try-- Refresh to start a new Game"}
+      </div>
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+      <HangmanWord reveal={isLoser} guessedLetters={guessedLetters} wordToGuess={wordToGuess}/>
+      <div className="self-stretch">
+        <Keyboard disabled={isWinner|| isLoser}
+                  activeLetters={guessedLetters.filter(letter=>wordToGuess.includes(letter))}
+                  inactiveLetters={incorrectLetters}
+                  addGuessedLetter={addGuessedLetter}/>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,38 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '50': '50px',
+        '10': '10px',
+        '100': '100px',
+      },
+      height: {
+        '50': '50px',
+        '100': '100px',
+        '10': '10px',
+      },
+      borderWidth: {
+        '10': '10px',
+        '0.1em': '.1em',
+      },
+      rotate: {
+        '-30': '-30deg',
+        '30': '30deg',
+        '60': '60deg',
+      },
+      transformOrigin: {
+        'left-bottom': 'left bottom',
+        'right-bottom': 'right bottom',
+      },
+      inset: {
+        '-30': '-30px',
+        '0': '0',
+        '10': '10px',
+        '90': '90px',
+      },
+    },
   },
+  variants: {},
   plugins: [],
 }
